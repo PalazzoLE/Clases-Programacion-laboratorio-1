69a005a3d3e0c8d3a9f0623019163c5e5e5d9dc3
alumno <alumno@lab6pc0>
2018-09-05 20:47:49 -0300

    clase 5b

--- clase05/main.c
+++ clase05/main.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "utn.h"
+#define CANT_EMPLEADOS 5
+
+int mostrarArray (int* pArray, int limite);
 
 int main()
 {
-    printf("Hello world!\n");
+    int edades[CANT_EMPLEADOS];
+    int i;
+
+    for (i=0;i< CANT_EMPLEADOS;i++)
+    {
+        if(utn_getNumero(&edades[i],2,"Edad?\t","Error!!!",0,200)==-1)
+        {
+            edades[i]=-1;
+        }
+    }
+
+    mostrarArray(edades,CANT_EMPLEADOS);
+    return 0;
+}
+
+int mostrarArray (int* pArray, int limite)
+{
+
+    int i;
+    for (i=0;i<limite;i++)
+    {
+        printf("\n%d",pArray[i]);
+    }
+
     return 0;
 }
